af19d479b0aa381de8f12290737e263fe4a74157
João Manoel <joaomanoel.ms.br@gmail.com>
2024-12-27 10:22:05 -0400

    create card route

---
+++ apps/api/src/http/routes/cards/create-card.ts
@@ -0,0 +1,48 @@
+import { auth } from "@/http/middlewares/auth";
+import { prisma } from "@/lib/prisma";
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { BadRequestError } from "../_errors/bad-request-error";
+import { BrandCardType } from "@prisma/client";
+
+export async function createCard(app: FastifyInstance){
+  app.withTypeProvider<ZodTypeProvider>()
+  .register(auth)
+  .post('/card', {
+    schema: {
+      security: [{ bearerAuth: [] }],
+      body: z.object({
+        name: z.string(),
+        brand: z.nativeEnum(BrandCardType),
+        limit: z.number(),
+      }),
+      response: {
+        201: z.object({
+          id: z.string()
+        })
+      },
+    }}, 
+    async (request, reply) => {
+      const userId = await request.getCurrentUserId()
+
+      const { name, brand, limit } = request.body
+
+
+      const card = await prisma.card.create({
+        data: {
+          name,
+          brand,
+          limit,
+          user: { connect: { id: userId } }
+        }
+      })
+
+      if(!card){
+        throw new BadRequestError('Could not create card')
+      }
+
+      return reply.status(201).send({ id: card.id })
+  
+  })
+}
