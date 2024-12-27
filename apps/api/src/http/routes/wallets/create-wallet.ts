3316df8965c1e22c24c732835ae090c5efae8409
João Manoel <joaomanoel.ms.br@gmail.com>
2024-12-27 10:06:30 -0400

    create route create wallet

---
+++ apps/api/src/http/routes/wallets/create-wallet.ts
@@ -0,0 +1,43 @@
+import { auth } from "@/http/middlewares/auth";
+import { prisma } from "@/lib/prisma";
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { BadRequestError } from "../_errors/bad-request-error";
+
+export async function createWallet(app: FastifyInstance){
+  app.withTypeProvider<ZodTypeProvider>()
+  .register(auth)
+  .post('/wallet', {
+    schema: {
+      security: [{ bearerAuth: [] }],
+      body: z.object({
+        name: z.string()
+      }),
+      response: {
+        201: z.object({
+          id: z.string()
+        })
+      },
+    }}, 
+    async (request, reply) => {
+      const userId = await request.getCurrentUserId()
+
+      const { name } = request.body
+
+
+      const wallet = await prisma.wallet.create({
+        data: {
+          name,
+          user: { connect: { id: userId } }
+        }
+      })
+
+      if(!wallet){
+        throw new BadRequestError('Could not create wallet')
+      }
+
+      return reply.status(201).send({ id: wallet.id })
+  
+  })
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -5,6 +5,7 @@ import { fastify } from "fastify"
 import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod"
 import { authenticateWithGoogle } from "./routes/auth/authenticate-with-google"
 import { getProfile } from "./routes/auth/get-profile"
+import { createWallet } from "./routes/wallets/create-wallet"
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -19,6 +20,8 @@ app.register(fastifyCors, { origin: "*" })
 
 app.register(authenticateWithGoogle)
 app.register(getProfile)
+
+app.register(createWallet)
 
 app.listen({port: env.PORT, host: '0.0.0.0'}).then(() => {
   console.log(`Server running on http://localhost:${env.PORT}`)
