4f39fca0d33d0cbe23c96715869d25afdb2145a7
João Manoel <joaomanoel.ms.br@gmail.com>
2024-12-29 16:39:09 -0400

    change text de termos de servicos

--- apps/web/src/components/register-form.tsx
+++ apps/web/src/components/register-form.tsx
@@ -57,8 +57,9 @@ export function RegisterForm({
         </div>
       </form>
       <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{' '}
-        and <a href="#">Privacy Policy</a>.
+        Ao clicar em continuar, você concorda com nossos{' '}
+        <a href="#">Termos de Serviços</a> e{' '}
+        <a href="#">Política de Privacidade</a>.
       </div>
     </div>
   )
