4ff69c224f8d94ba5b80d1098a99a73d67f3bf97
joao-manoel <joaomanoel.ms.br@gmail.com>
2025-06-15 03:52:44 -0400

    test

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/http/server.ts'], // ponto de entrada principal
-  format: ['esm'], // gerar ESM (usando "type": "module")
+  entry: ['src/http/server.ts'],
+  format: ['esm'],
   target: 'node20',
   outDir: 'dist',
   sourcemap: true,
   clean: true,
   minify: true,
-  dts: true, // gera .d.ts
-  noExternal: ['@ck/env'], // inclui no bundle
+  dts: true,
+  noExternal: ['@ck/env'],
 })
