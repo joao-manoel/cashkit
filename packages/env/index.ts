147630ac1296f4ed731330708442ee84863da55d
joao-manoel <joaomanoel.ms.br@gmail.com>
2025-06-15 13:51:26 -0400

    set default envs

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -4,21 +4,29 @@ import { z } from 'zod'
 export const env = createEnv({
   server: {
     PORT: z.coerce.number().default(1010),
-    JWT_SECRET: z.string(),
+    JWT_SECRET: z.string().default('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'),
 
-    GOOGLE_OAUTH_CLIENT_SECRET: z.string(),
-    GOOGLE_OAUTH_CLIENT_ID: z.string(),
-    GOOGLE_OAUTH_REDIRECT_URI: z.string(),
+    GOOGLE_OAUTH_CLIENT_SECRET: z
+      .string()
+      .default('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'),
+    GOOGLE_OAUTH_CLIENT_ID: z
+      .string()
+      .default(
+        '1070787267108-p7hl80m3tpeoakbtnf8e4dc4dh0cs8vc.apps.googleusercontent.com'
+      ),
+    GOOGLE_OAUTH_REDIRECT_URI: z
+      .string()
+      .default('https://meubolso.site/api/auth/callback'),
 
     REDIS_HOST: z.string().default('redis'),
     REDIS_PORT: z.coerce.number().default(6379),
 
-    NODEMAILER_USER: z.string(),
-    NODEMAILER_PASSWORD: z.string(),
+    NODEMAILER_USER: z.string().default('jmsneto120@gmail.com'),
+    NODEMAILER_PASSWORD: z.string().default('fpxr bvoo xafd jlrr'),
   },
   client: {},
   shared: {
-    NEXT_PUBLIC_API_URL: z.string(),
+    NEXT_PUBLIC_API_URL: z.string().default('https://api.meubolso.site'),
   },
   runtimeEnv: {
     PORT: process.env.PORT,
